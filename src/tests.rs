3b155651a434f2bf8512517bd0ca728fd81d871d
barrelll <jharrell_@outlook.com>
2018-10-28 14:10:37 -0500

    back to working on tests for iods and descriptors

--- src/tests.rs
+++ src/tests.rs
@@ -1,20 +1,34 @@
 #![cfg(test)]
+use std::{fs::File, path::Path, path::PathBuf};
+use MediaStreamTree;
 
-#[test]
-fn file_stream() {
-    use std::{fs::File, path::Path};
-    use MediaStreamTree;
-    let cargoman = env!("CARGO_MANIFEST_DIR");
-    let path = Path::new(cargoman)
-        .join("..")
-        .join("..")
+fn path(filename: &str) -> PathBuf {
+    /*D:\download.tsi.telecom-paristech.fr\gpac\MPEG\ISOBMFF-Conformance\isobmff*/
+    let path = Path::new("d:\\")
         .join("download.tsi.telecom-paristech.fr")
         .join("gpac")
         .join("MPEG")
         .join("ISOBMFF-Conformance")
         .join("isobmff")
-        .join("01_simple.mp4");
-    let mut handle = File::open(path).expect("Error opening file");
+        .join(filename);
+    path.to_path_buf()
+}
+
+fn handle(filename: &str) -> File {
+
+    File::open(path(filename)).expect("Error opening file")
+}
+
+#[test]
+fn file_stream() {
+    let mut handle = handle("13_long.mp4");
     let node = handle.searchtree_stype::<super::iso_p12::Ftyp>("ftyp");
+    println!("Node {:?}", node);
+}
+
+#[test]
+fn iods() {
+    let mut handle = handle("01_simple.mp4");
+    let node = handle.searchtree_stype::<super::iso_p14::Iods>("moov.iods");
     println!("Node {:?}", node);
 }
