71c8544b004b36725bd44b8aa49620b69990e9bb
barrelll <jharrell_@outlook.com>
2018-11-02 21:50:13 -0500

    finished initialobjectdescriptor

--- src/base_descriptors/initobjdescr.rs
+++ src/base_descriptors/initobjdescr.rs
@@ -1,4 +1,4 @@
-use super::{es_id_inc::ESIDInc, DescrBase, DescrBaseTags, DescrBuilder};
+use super::{es_id_inc::ESIDInc, size_of_instance, DescrBase, DescrBaseTags, DescrBuilder};
 use IsSlice;
 #[repr(align(8))]
 #[derive(Debug, Default, Clone)]
@@ -6,7 +6,7 @@ pub struct InitialObjectDescriptor {
     tag: Option<DescrBaseTags>,
     od_id: Option<[bool; 10]>,
     url_flag: Option<bool>,
-    reserved: Option<[bool; 5]>,
+    reserved: Option<[bool; 4]>,
     url_length: Option<u8>,
     url_string: Option<String>,
     include_inline_profile_level_flag: Option<bool>,
@@ -21,7 +21,7 @@ pub struct InitialObjectDescriptor {
 impl DescrBuilder for InitialObjectDescriptor {
     fn build<T: IsSlice<Item = u8>>(d: T) -> Option<Self> {
         let data = d.as_slice();
-        use byteorder::{ReadBytesExt, BigEndian};
+        use byteorder::{BigEndian, ReadBytesExt};
         use std::io::Cursor;
         let tag = Some(match Cursor::new(&data[..1]).read_u8().unwrap() {
             0x02 => DescrBaseTags::InitialObjectDescrTag,
@@ -31,28 +31,82 @@ impl DescrBuilder for InitialObjectDescriptor {
             }
         });
 
-        let mut cursor = 1;
-        let mut size_of_instance = 0;
-        let mut next_byte = true;
-        while next_byte {
-            let size = Cursor::new(&data[cursor..cursor+1]).read_u8().unwrap();
-            cursor+=1;
-            next_byte = size & (1 << 7) > 0;
-            size_of_instance = size_of_instance<<7 | size;
-        }
+        let mut cursor: usize = 1;
+        let _ = size_of_instance(data, &mut cursor);
+        let id = Cursor::new(&data[cursor..cursor + 2])
+            .read_u16::<BigEndian>()
+            .unwrap();
 
-        let id = Cursor::new(&data[inner_cursor..inner_cursor+2]).read_u16::<BigEndian>().unwrap();
-        for i in 0..10 {
-            let val = id & (1 << i) > 0;
-            print!("{:?},", val);
+        let od_id = Some({
+            let mut arr = [false; 10];
+            let mut arr_idx = 0;
+            for i in (6..16).rev() {
+                let val = id & (1 << i) > 0;
+                arr[arr_idx] = val;
+                arr_idx += 1;
+            }
+            arr
+        });
+
+        let include_inline_profile_level_flag = Some({ id & (1 << 4) > 0 });
+
+        let url_flag = Some({ id & (1 << 5) > 0 });
+
+        match url_flag {
+            Some(v) => {
+                let reserved = Some([true; 4]);
+                let mut url_length = None;
+                let mut url_string = None;
+                let mut od_profile_level_indication = None;
+                let mut scene_profile_level_indication = None;
+                let mut audio_profile_level_indication = None;
+                let mut visual_profile_level_indication = None;
+                let mut graphics_profile_level_indication = None;
+                if v {
+                    url_length = match Cursor::new(&data[cursor + 2..cursor + 3]).read_u8() {
+                        Ok(val) => {
+                            url_string = match String::from_utf8(
+                                data[cursor + 3..cursor + (val as usize)].to_vec(),
+                            ) {
+                                Ok(val) => Some(val),
+                                Err(e) => {
+                                    panic!("InitialObjectDescriptor error, cannot parse url_string. {:?}", e);
+                                },
+                            };
+                            Some(val)
+                        }
+                        Err(e) => {
+                            panic!("InitialObjectDescriptor error, cannot parse url_length. {:?}", e);
+                        },
+                    };
+                } else {
+                    od_profile_level_indication = Cursor::new(&data[cursor + 2..cursor + 3]).read_u8().ok();
+                    scene_profile_level_indication = Cursor::new(&data[cursor + 3..cursor + 4]).read_u8().ok();
+                    audio_profile_level_indication = Cursor::new(&data[cursor + 4..cursor + 5]).read_u8().ok();
+                    visual_profile_level_indication = Cursor::new(&data[cursor + 5..cursor + 6]).read_u8().ok();
+                    graphics_profile_level_indication = Cursor::new(&data[cursor + 6..cursor + 7]).read_u8().ok();
+                }
+                let descr = Box::new(ESIDInc::build(data).unwrap()) as Box<DescrBase>;
+                let descriptors = vec![descr];
+                Some(InitialObjectDescriptor {
+                    tag,
+                    od_id,
+                    url_flag,
+                    url_length,
+                    url_string,
+                    reserved,
+                    include_inline_profile_level_flag,
+                    od_profile_level_indication,
+                    scene_profile_level_indication,
+                    audio_profile_level_indication,
+                    visual_profile_level_indication,
+                    graphics_profile_level_indication,
+                    descriptors,
+                })
+            }
+            None => {
+                panic!("Can't find InitialObjectDescriptor url flag!");
+            }
         }
-        let descr = Box::new(ESIDInc::build(data).unwrap()) as Box<DescrBase>;
-        let _ = vec![descr];
-        let iod = InitialObjectDescriptor {
-            tag,
-            ..Default::default()
-        };
-        println!("\n{:?}\n", iod);
-        None
     }
 }
--- src/base_descriptors/mod.rs
+++ src/base_descriptors/mod.rs
@@ -114,4 +114,20 @@ pub trait DescrBuilder {
         Self: Sized;
 }
 
+fn size_of_instance(data: &[u8], cursor: &mut usize) -> u8 {
+    let mut c = *cursor;
+    use byteorder::ReadBytesExt;
+    use std::io::Cursor;
+    let mut size_of_instance = 0;
+    let mut next_byte = true;
+    while next_byte {
+        let size = Cursor::new(&data[c..c + 1]).read_u8().unwrap();
+        c += 1;
+        next_byte = size & (1 << 7) > 0;
+        size_of_instance = size_of_instance << 7 | size;
+    }
+    *cursor = c;
+    size_of_instance
+}
+
 fn _descrfactory() {}
