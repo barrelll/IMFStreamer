fa4fca0c12fb720cf8b99d32e9311aec7d4dc986
barrelll <jharrell_@outlook.com>
2018-11-25 15:11:41 -0600

    added full box to esds

--- src/iso_p14/esds.rs
+++ src/iso_p14/esds.rs
@@ -1,9 +1,10 @@
 use base_descriptors::{DescrBuilder, ESDescriptor};
-use {BuildNode, Name};
+use {BuildNode, Name, FullBox};
 
 #[repr(align(8))]
 #[derive(Debug, Default, Clone)]
 pub struct Esds {
+    pub fullbox: Option<FullBox>,
     pub od: Option<ESDescriptor>,
 }
 
@@ -15,7 +16,11 @@ impl<'a> Name<'a> for Esds {
 
 impl BuildNode for Esds {
     fn build(data: &[u8]) -> Option<Self> {
+        let fullbox = FullBox::from(&data[8..12]).ok();
         let od = ESDescriptor::build(&data[12..]);
-        Some(Esds { od })
+        Some(Esds {
+            fullbox,
+            od,
+        })
     }
 }
